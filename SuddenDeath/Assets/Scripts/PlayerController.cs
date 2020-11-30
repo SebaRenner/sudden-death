3f7e192a43102e6f2d2f80c8391a65ab0cfffe97
Nick <nick.broennimann@students.fhnw.ch>
2020-11-30 16:19:02 +0100

    Implemented Player movement based on brakeys tutorial.

---
+++ SuddenDeath/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
+
+    private PlayerMotor motor;
+
+    public void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    public void Update()
+    {
+        float xMovement = Input.GetAxisRaw("Horizontal"); // -1 and 1
+        float zMovement = Input.GetAxisRaw("Vertical"); // -1 and 1
+
+        Vector3 moveHorizontal = transform.right * xMovement;
+        Vector3 moveVertical = transform.forward * zMovement;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        motor.move(velocity);
+
+        float yRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 rotation = new Vector3(0f, yRotation, 0f) * lookSensitivity;
+
+        motor.rotate(rotation);
+
+        float xRotation = Input.GetAxisRaw("Mouse Y");
+        Vector3 cameraRotation = new Vector3(xRotation, 0, 0) * lookSensitivity;
+
+        motor.rotateCamera(cameraRotation);
+    }
+}
---
+++ SuddenDeath/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+    [SerializeField]
+    private bool invertCamera = false;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
+
+    private Rigidbody rb;
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void move(Vector3 velocity)
+    {
+        this.velocity = velocity;
+    }
+
+    public void rotate(Vector3 rotation)
+    {
+        this.rotation = rotation;
+    }
+
+    public void rotateCamera(Vector3 cameraRotation)
+    {
+        this.cameraRotation = cameraRotation;
+    }
+
+    public void FixedUpdate()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+
+        if (cam != null)
+        {
+            cam.transform.Rotate(!invertCamera ? -cameraRotation : cameraRotation);
+        }
+    }
+}

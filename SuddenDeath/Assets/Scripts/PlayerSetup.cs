dcbc581e493c4bae37b97633f515b5e46f2238e6
Nick Brönnimann <nick.broennimann@gmail.com>
2020-11-30 22:46:27 +0100

    got networking up and running.

---
+++ SuddenDeath/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    Camera sceneCamera;
+
+    public void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            foreach (Behaviour b in componentsToDisable)
+            {
+                b.enabled = false;
+            }
+        } else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void onDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}

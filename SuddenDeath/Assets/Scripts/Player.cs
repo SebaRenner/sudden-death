c276a6d37557ea50071adf51e50ba426a6bf3a9f
Sebastian Renner <sebastian.renner@students.fhnw.ch>
2020-12-01 16:47:53 +0100

    Player Health, Taking Damage and GameManager implemented

---
+++ SuddenDeath/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    [SerializeField]
+    private int maxHealth = 100;
+
+    // Annotation SyncVar to synchronize health from server to all clients
+    [SyncVar]
+    private int currentHealth;
+
+    void Awake()
+    {
+        SetDefaults();
+    }
+
+
+    public void TakeDamage(int _amount)
+    {
+        currentHealth -= _amount;
+        Debug.Log(transform.name + " new has " + currentHealth + " health.");
+    }
+
+    public void SetDefaults()
+    {
+        currentHealth = maxHealth;
+    }
+
+}
